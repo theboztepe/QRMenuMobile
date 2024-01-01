b93adc885849f1b791f1bbf738d54405f7f65abe
Çağrı <theboztepe@gmail.com>
2024-01-01 19:06:29 +0300

    Add project files.

---
+++ QRMenuMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QRMenuMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QRMenuMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QRMenuMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
